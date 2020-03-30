66952398747484383c63ff29194c4bb6780d352b
Your Name <you@example.com>
2020-03-30 19:32:49 +0300

    homework 2 complete

--- src/main/java/org/example/homeworks/hw2/Practice2.java
+++
@@ -1,4 +0,0 @@
-package org.example.homeworks.hw2;
-
-public class Practice2g {
-}
---
+++ src/main/java/org/example/homeworks/hw2/PracticeTwo.java
@@ -0,0 +1,7 @@
+package org.example.homeworks.hw2;
+
+public class PracticeTwo {
+    public static void main(String[] args) {
+
+    }
+}
