f9d0ffd1642ce4fc8fcffcaeaf3be98eb1a2613b
Vlad <pasukvlad0@gmail.com>
2020-04-12 19:25:05 +0300

    homework 2 complete

---
+++ src/main/java/lecture_6/Library.java
@@ -0,0 +1,13 @@
+package lecture_6;
+
+public class Library {
+    public static void main(String[] args) {
+        Author ws = new Author("William", "Chakespeare");
+        Author ws1 = new Author("William", "Chakespear");
+        System.out.println(ws.equals(ws1));
+        Book b = new Book("1", "Hamlet", ws, "1600", "some description", "123");
+        Book b1 = new Book("2", "Romeo & Juliette", ws, "1595", "some description", "345345");
+        System.out.println(b);
+        System.out.println(b1);
+    }
+}
